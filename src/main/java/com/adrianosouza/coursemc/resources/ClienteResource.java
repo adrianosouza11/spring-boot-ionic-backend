26c5689ac0d61fbd98deccd12724e2d5288f67c0
Adriano O. Souza <adrianosouza11@gmail.com>
2019-03-13 23:42:41 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/adrianosouza/coursemc/resources/ClienteResource.java
@@ -0,0 +1,25 @@
+package com.adrianosouza.coursemc.resources;
+
+import com.adrianosouza.coursemc.domain.Categoria;
+import com.adrianosouza.coursemc.domain.Cliente;
+import com.adrianosouza.coursemc.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Cliente cliente = service.buscar(id);
+		return ResponseEntity.ok().body(cliente);
+	}
+}
---
+++ src/main/java/com/adrianosouza/coursemc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.adrianosouza.coursemc.services;
+
+import com.adrianosouza.coursemc.domain.Cliente;
+import com.adrianosouza.coursemc.repositories.ClienteRepository;
+import com.adrianosouza.coursemc.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repository;
+	
+	public Cliente buscar(Integer id){
+		Optional<Cliente> cliente = repository.findById(id);
+		
+		return cliente.orElseThrow(() -> {
+			return new ObjectNotFoundException("Objeto não encontrado ID " + id + ", tipo: " + Cliente.class.getName());
+		});
+	}
+}
